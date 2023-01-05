75758ef465b0f97c8e1e997eed70abab43419b69
saymanrabbi <mdrabbi.asm@gmail.com>
2023-01-05 12:27:05 +0600

    initial setup

--- Server.js
+++ Server.js
@@ -4,6 +4,17 @@ const colors = require('colors');
 
 
 // Connect to MongoDB
-const connectDB = async () => {
-    
+export const connectDB =  () => {
+    try {
+        const conn =  mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    } catch (err) {
+        console.error(`Error: ${err.message}`.red);
+        process.exit(1);
+    }
 }
--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const { connectDB } = require('./Server');
+const { ErrorHandeler } = require('./Middleware/ErrorHandeler');
 const app = express();
 const dotenv = require('dotenv').config();
-
+const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+/* start the server */
+connectDB()
+// routes
+
+/*Lising to the port*/
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
+/*existing nothitng*/
+app.all('*', (req, res) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    });
+})
+/*error handeler*/
+app.use(ErrorHandeler);
